8bd30c3728d99c9ece85e8592ff05c11fefe3b5c
Александр Зимарев <livetoslide@icloud.com>
2025-03-29 13:29:41 +0300

    Initial Commit

---
+++ SportHelper/SportHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportHelperApp.swift
+//  SportHelper
+//
+//  Created by Александр Зимарев on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SportHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SportHelperTests/SportHelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  SportHelperTests.swift
+//  SportHelperTests
+//
+//  Created by Александр Зимарев on 29.03.2025.
+//
+
+import Testing
+@testable import SportHelper
+
+struct SportHelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
